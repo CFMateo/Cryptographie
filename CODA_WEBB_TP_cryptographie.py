886fc60eb1231432daddaefc4ed549e2c1626924
Hexa <84851013+ctrl-hexa@users.noreply.github.com>
2023-05-14 10:11:24 +0200

    Q1

---
+++ CODA_WEBB_TP_cryptographie.py
@@ -0,0 +1,25 @@
+""" Utiliser doctests"""
+from doctest import doctest
+
+# ? Question 1
+
+# A = 0, B = 0 --> XOR(A,B) = 0 donc XOR(XOR(A,B),B) = XOR(0,0) = 0
+# A = 1, B = 0 --> XOR(A,B) = 1 donc XOR(XOR(A,B),B) = XOR(1,0) = 1
+# A = 0, B = 1 --> XOR(A,B) = 1 donc XOR(XOR(A,B),B) = XOR(1,1) = 0
+# A = 1, B = 1 --> XOR(A,B) = 0 donc XOR(XOR(A,B),B) = XOR(0,1) = 1
+# On observe bien que les valeurs de A sont exactement les mêmes pour XOR(XOR(A,B),B).
+
+
+# ? Question 2
+# ? Question 3
+# ? Question 4
+# ? Question 5
+# ? Question 6
+# ? Question 7
+# ? Question 8
+# ? Question 9
+# ? Question 10
+# ? Question 11
+# ? Question 12
+# ? Question 13
+# ? Question 14
